ba84b0dd990faabbd797810ebe66c6d7070c3d30
Ariel Corrado <jarielc2012@yahoo.com.ar>
2022-10-03 14:51:01 -0300

    se agregó categoria Air Coolers

--- src/components/navbar/Dropdown.jsx
+++ src/components/navbar/Dropdown.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dropdown = () => {
     return (
         <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="index.html" role="button" aria-haspopup="true" aria-expanded="false">Refrigeración</a>
             <div className="dropdown-menu text-center">
-                <a className="dropdown-item" href="index.html">Air Cooling</a>
+                <Link className="dropdown-item" to={"/category/air_coolers"}>Air Cooling</Link>
                 <div className="dropdown-divider" />
                 <a className="dropdown-item" href="index.html">Water Cooling</a>
             </div>
