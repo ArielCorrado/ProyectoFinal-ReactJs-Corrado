e4a592a1d8f44a7db36dfb72af3319756b8d4a93
Ariel Corrado <jarielc2012@yahoo.com.ar>
2022-10-05 01:22:26 -0300

    busquedas agregadas con exito

--- src/components/navbar/Busquedas.jsx
+++ src/components/navbar/Busquedas.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import CartWidget from './CartWidget';
 import Logo from './Logo';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Busquedas = () => {
+
+    const [link, setLink] = useState ("");
+
     return (
         <div className="contBusquedas flex">
             <Logo />
-            <div className="contBusquedaIcono flex">
-                <input className="campoBusquedas" type="search" placeholder="Buscar Producto" />
-                <Link to={`/search/amd`}><button className="botonBusquedas" type="submit">Buscar</button></Link>
+            <div className='flex'>
+                <form action={`/search/${link}`} method="get" className="contBusquedaIcono flex" onSubmit={(e) => setLink(e.target.firstChild.value)}>
+                    <input className="campoBusquedas" type="search" placeholder="Buscar Producto" />
+                    <button className="botonBusquedas" type="submit">Buscar</button>
+                </form>    
                 <CartWidget />
-            </div>    
+            </div>
         </div>
     );
 }
