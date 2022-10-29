5c790d1a92cb5d9afbab28caa4b6c6fea2e41570
Ariel Corrado <jarielc2012@yahoo.com.ar>
2022-10-28 21:31:42 -0300

    cambiando configuracion git ignorecase false

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { memo } from 'react';
+
+const Footer = () => {
+    return (
+        <div className='contFooter'>
+            <div className="flex column cont_newsLetter">
+                <div className='flex'>
+                    <p className='ofertas'>Recibí ofertas exclusivas!</p>
+                </div> 
+                <div>
+                    <input className="inputNewsLetter" type="email" placeholder="email" />
+                    <button className="botonNewsLetter">Submit</button>
+                </div>
+            </div> 
+            <ul className='contFooter__ul flex'>
+                <li className='contFooter__ul__li' key="Nosotros"> Nosotros </li>
+                <li className='contFooter__ul__li' key="Contacto"> Contacto </li>
+                <li className='contFooter__ul__li' key="Soporte"> Soporte </li>
+                <li className='contFooter__ul__li' key="Ubicación"> Ubicación </li>
+            </ul>         
+            <div className='footer__contImg flex'>
+                <img className='footer_img' src="../images/facebook.png" alt="" />
+                <img className='footer_img' src="../images/ws.png" alt="" />
+                <img className='footer_img' src="../images/instagram.png" alt="" />
+            </div>    
+        </div>
+    );
+}
+
+export default memo (Footer);
