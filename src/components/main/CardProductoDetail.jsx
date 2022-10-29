c7380e62f4da9ee69e2dfcd9e72de8ce0e118646
Ariel Corrado <jarielc2012@yahoo.com.ar>
2022-10-28 21:44:06 -0300

    importando swal sweetalert por error en vercel

--- src/components/main/CardProductoDetail.jsx
+++ src/components/main/CardProductoDetail.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { CarritoContext } from '../../context/CarritoContext';
 import { useContext, useState } from 'react';
+import Swal from 'sweetalert2';
 
 const CardProductoDetail = ({producto}) => {
 
