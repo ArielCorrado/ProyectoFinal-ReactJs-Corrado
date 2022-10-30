edc6ce63d30471b39cc283eb996663a3d4375608
Ariel Corrado <jarielc2012@yahoo.com.ar>
2022-10-30 12:47:18 -0300

    proyecto final terminado

--- src/components/main/Filtro.jsx
+++ src/components/main/Filtro.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { error } from '../../utils/funcionesUtiles';
 
 const arrayFiltro = [];
 
